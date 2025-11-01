bdce4c8b5b48cde5554a200fbed320047a456982
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-11-01 20:43:03 +0000

    task9

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Find max value */
+/* Find maximum value in the array */
 int get_max(int *array, size_t size)
 {
     int max;
@@ -10,35 +10,43 @@ int get_max(int *array, size_t size)
 
     max = array[0];
     for (i = 1; i < size; i++)
+    {
         if (array[i] > max)
             max = array[i];
+    }
+
     return max;
 }
 
-/* Counting sort for radix */
+/* Counting sort based on current digit */
 void counting_sort_for_radix(int *array, size_t size, int exp)
 {
     int *output;
     int count[10];
     size_t i;
-    int j, digit;
+    int digit;
 
+    /* Allocate memory for output array */
     output = malloc(sizeof(int) * size);
     if (!output)
         return;
 
+    /* Initialize count array to 0 */
     for (i = 0; i < 10; i++)
         count[i] = 0;
 
+    /* Count occurrences of digits */
     for (i = 0; i < size; i++)
     {
         digit = (array[i] / exp) % 10;
         count[digit]++;
     }
 
+    /* Update count[i] to store actual positions */
     for (i = 1; i < 10; i++)
         count[i] += count[i - 1];
 
+    /* Build the output array (traverse from end) */
     for (i = size; i > 0; i--)
     {
         digit = (array[i - 1] / exp) % 10;
@@ -46,13 +54,14 @@ void counting_sort_for_radix(int *array, size_t size, int exp)
         count[digit]--;
     }
 
+    /* Copy output back to original array */
     for (i = 0; i < size; i++)
         array[i] = output[i];
 
     free(output);
 }
 
-/* Radix sort */
+/* LSD Radix Sort */
 void radix_sort(int *array, size_t size)
 {
     int max;
