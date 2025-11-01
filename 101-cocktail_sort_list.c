40c3ca73cc8a728e52fff4c5f955ee73ecbc680d
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-11-01 20:54:04 +0000

    task5

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -23,61 +23,79 @@ void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
 }
 
 /**
+ * forward_pass - performs a forward pass of cocktail shaker sort
+ * @list: pointer to the head of the list
+ * @start: starting node
+ * @end: ending node
+ * Return: pointer to the last node after pass
+ */
+listint_t *forward_pass(listint_t **list, listint_t *start, listint_t *end)
+{
+	listint_t *current;
+
+	current = start;
+	while (current->next != end)
+	{
+		if (current->n > current->next->n)
+		{
+			swap_nodes(list, current, current->next);
+			print_list(*list);
+		}
+		else
+			current = current->next;
+	}
+	return (current);
+}
+
+/**
+ * backward_pass - performs a backward pass of cocktail shaker sort
+ * @list: pointer to the head of the list
+ * @start: starting node
+ * @end: ending node
+ * Return: pointer to the first node after pass
+ */
+listint_t *backward_pass(listint_t **list, listint_t *start, listint_t *end)
+{
+	listint_t *current;
+
+	current = end;
+	while (current && current->prev != start->prev)
+	{
+		if (current->n < current->prev->n)
+		{
+			swap_nodes(list, current->prev, current);
+			print_list(*list);
+		}
+		else
+			current = current->prev;
+	}
+	return (current);
+}
+
+/**
  * cocktail_sort_list - sorts a doubly linked list in ascending order
  *                     using the Cocktail shaker sort algorithm
  * @list: pointer to the head of the list
  */
 void cocktail_sort_list(listint_t **list)
 {
-	int swapped;
 	listint_t *start;
 	listint_t *end;
-	listint_t *current;
 
 	if (!list || !*list || !(*list)->next)
 		return;
 
 	start = *list;
 	end = NULL;
-	swapped = 1;
 
-	while (swapped)
+	while (1)
 	{
-		swapped = 0;
-		current = start;
-
-		/* forward pass */
-		while (current->next != end)
-		{
-			if (current->n > current->next->n)
-			{
-				swap_nodes(list, current, current->next);
-				print_list(*list);
-				swapped = 1;
-			}
-			else
-				current = current->next;
-		}
-		end = current;
-
-		if (!swapped)
+		end = forward_pass(list, start, end);
+		if (!start || start == end)
 			break;
 
-		swapped = 0;
-		current = end->prev;
-
-		/* backward pass */
-		while (current && current->prev)
-		{
-			if (current->n < current->prev->n)
-			{
-				swap_nodes(list, current->prev, current);
-				print_list(*list);
-				swapped = 1;
-			}
-			else
-				current = current->prev;
-		}
-		start = current ? current->next : *list;
+		start = backward_pass(list, start, end);
+		if (!end || start == end)
+			break;
 	}
 }
