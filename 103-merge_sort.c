90679e675ef944d9461c54c18601b9d8d2ecd62a
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-11-01 20:27:45 +0000

    task7

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -2,14 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * merge - Merge two subarrays into one sorted array
- * @array: The main array
- * @left: Start index of left subarray
- * @mid: End index of left subarray
- * @right: End index of right subarray
- * @temp: Temporary array used for merging
- */
 void merge(int *array, int left, int mid, int right, int *temp)
 {
     int i, j, k, x;
@@ -62,13 +54,6 @@ void merge(int *array, int left, int mid, int right, int *temp)
     printf("\n");
 }
 
-/**
- * merge_sort_rec - Recursive top-down merge sort
- * @array: The array to sort
- * @left: Left index
- * @right: Right index
- * @temp: Temporary array for merging
- */
 void merge_sort_rec(int *array, int left, int right, int *temp)
 {
     int mid;
@@ -78,16 +63,14 @@ void merge_sort_rec(int *array, int left, int right, int *temp)
 
     mid = left + (right - left) / 2;
 
+    /* Sort left first */
     merge_sort_rec(array, left, mid, temp);
+    /* Sort right second */
     merge_sort_rec(array, mid + 1, right, temp);
+    /* Merge left and right */
     merge(array, left, mid, right, temp);
 }
 
-/**
- * merge_sort - Top-down Merge Sort algorithm
- * @array: Array to sort
- * @size: Size of the array
- */
 void merge_sort(int *array, size_t size)
 {
     int *temp;
