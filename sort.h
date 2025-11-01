fb0e34a46420fb6066e3653bcbae0ef2c1c46e4e
Hussam <samdevy@outlook.com>
2025-11-01 22:26:36 +0300

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+/* Helper function provided by project */
+void print_array(const int *array, size_t size);
+
+/* Sorting function */
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
